82f631c2328f916d85ceae50550abc200eb9bef3
herragras <herragras@users.noreply.github.com>
2021-02-07 00:53:01 +0100

    resim ve collapse özelligi eklendi

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Card = (props) => {
     return(
         <div className="card">
-        <img src="..." className="card-img-top" alt="..."/>
+        <img src={props.image} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{props.cardTitle}</h5>
                 <p className="card-text">{props.cardText}</p>
--- src/index.js
+++ src/index.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from './components/Card';
+import Collapse from './components/Collapse';
 
 const App = () => {
     return (
         <div>
             <div className="card-group">
-               <Card 
-               cardTitle="Ankara"
-               cardText="This is the first card with supporting text below"
-               updatedTime="1 min ago"/>
-               <Card 
-               cardTitle="istanbul"
-               cardText="This is the second card with supporting text below"
-               updatedTime="2 min ago"/>
-               <Card 
-               cardTitle="Manisa"
-               cardText="This is the third card with supporting text below "
-               updatedTime="3 min ago"/>
+                <Collapse href="collapseExample1">
+                    <Card
+                        cardTitle="NASA"
+                        cardText="This is the first card with supporting text below"
+                        updatedTime="1 min ago"
+                        image="https://picsum.photos/id/1002/200/300" />
+                </Collapse>
+
+                <Collapse href="collapseExample2">
+                    <Card
+                        cardTitle="Gazelle"
+                        cardText="This is the second card with supporting text below"
+                        updatedTime="2 min ago"
+                        image="https://picsum.photos/id/1003/200/300" />
+                </Collapse>
+
+                <Collapse href="collapseExample3">
+                    <Card
+                        cardTitle="Winter"
+                        cardText="This is the third card with supporting text below "
+                        updatedTime="3 min ago"
+                        image="https://picsum.photos/id/1004/200/300" />
+                </Collapse>
             </div>
         </div>
     );
